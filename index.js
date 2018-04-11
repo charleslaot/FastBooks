584d348c9ce0529d2d2e21b75df981efcb0dc965
Carlos <krloslao90@gmail.com>
2018-04-11 08:07:09 -0400

    Code improvement

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 'use strict'
 
-// Globals
+// GLOBALS
+
 const GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes";
 const NYT_API_URL = "https://api.nytimes.com/svc/books/v3/lists.json?api-key=ecb23c2aa6254b85b8623e1916c960f3";
-
-var searchIndex = 0;
-var query = '';
 
 const sections = [
     "business-books",
     "science",
-    "combined-print-and-e-book-fiction",
-    "combined-print-and-e-book-nonfiction",
-    "sports",
-    "childrens-middle-grade-hardcover",
-    "young-adult-hardcover"
+    // "combined-print-and-e-book-fiction",
+    // "combined-print-and-e-book-nonfiction",
+    // "sports",
+    // "childrens-middle-grade-hardcover",
+    // "young-adult-hardcover"
 ]
 
+var searchIndex = 0;
+var query = '';
+
 var googleAjaxData = {
-    maxResults: 40,
+    maxResults: 7,
     printType: "books",
     startIndex: searchIndex,
     q: query,
-    key: 'AIzaSyCov1wfUCO9qudwOjH8D8F4EgNBEGRlGV0'
+    key: 'AIzaSyAsHwxYnlY3l5jV1JfvefLdIM5f4USJlL0'
 }
 
 var nyAjaxData = {
     list: ''
 }
 
-
 // GOOGLE API
+
 function getBooksFromAPI(api_url, ajaxData, callback) {
     const settings = {
         url: api_url,
@@ -50,7 +51,6 @@ function checkForItemsReceived(item) {
         return true;
     }
 }
-
 
 function normalizeSearchData(item) {
     let bookElement = {
@@ -85,8 +85,6 @@ function normalizeSearchData(item) {
     return bookElement;
 }
 
-
-// Passing the results to HTML
 function displaySearchData(data) {
     if (checkForItemsReceived(data)) {
         const results = data.items.map((item, index) => {
@@ -97,30 +95,22 @@ function displaySearchData(data) {
     }
 }
 
-// on submit
-function watchSubmit() {
-    $(".js-mainHeader").click(event => {
-        $("form input").val('');
-        renderEmptyForm();
-        showBestSeller();
+function infiniteScroll() {
+    var win = $(window);
+    win.scroll(function () {
+        if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+            query = $('.js-form').find('.search-field').val();
+            googleAjaxData.q = query;
+            if (googleAjaxData.q !== '') {
+                searchIndex += 40;
+                googleAjaxData.startIndex = searchIndex;
+                getBooksFromAPI(GOOGLE_BOOKS_API_URL, googleAjaxData, displaySearchData);
+            }
+        }
     });
+};
 
-    $('.js-searchSubmit').click(event => {
-        $(".js-form").submit();
-    })
 
-    $('.js-form').submit(event => {
-        event.preventDefault();
-        query = $(event.currentTarget).find('.search-field').val();
-        searchIndex = 0;
-        googleAjaxData.q = query;
-        googleAjaxData.startIndex = searchIndex;
-        renderEmptyForm();
-        getBooksFromAPI(GOOGLE_BOOKS_API_URL, googleAjaxData, displaySearchData);
-    });
-}
-
-// speech functionality
 function speechRecognition() {
     $("form").on('click', '.js-voice-search', function (event) {
         event.preventDefault();
@@ -151,23 +141,41 @@ function startDictation() {
     }
 }
 
-// infinite scroll 
-function infiniteScroll() {
-    var win = $(window);
-    win.scroll(function () {
-        if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
-            query = $('.js-form').find('.search-field').val();
-            googleAjaxData.q = query;
-            if (googleAjaxData.q !== '') {
-                searchIndex += 40;
-                googleAjaxData.startIndex = searchIndex;
-                getBooksFromAPI(GOOGLE_BOOKS_API_URL, googleAjaxData, displaySearchData);
-            }
-        }
+function lightboxScroll() {
+    $('.book-container').on('click', 'img', function (event) {
+        $('html').css('overflow', 'hidden');
+    })
+    $('.book-container').on('click', '.fa-close', function (event) {
+        $('html').css('overflow', 'visible');
+    })
+}
+
+function initEventHandler() {
+    $(".js-mainHeader").click(event => {
+        $("form input").val('');
+        renderEmptyForm();
+        showBestSeller();
     });
-};
+
+    $('.js-searchSubmit').click(event => {
+        $(".js-form").submit();
+    })
+
+    $('.js-form').submit(event => {
+        event.preventDefault();
+        query = $(event.currentTarget).find('.search-field').val();
+        renderEmptyForm();
+        searchIndex = 0;
+        googleAjaxData.q = query;
+        googleAjaxData.startIndex = searchIndex;
+        getBooksFromAPI(GOOGLE_BOOKS_API_URL, googleAjaxData, displaySearchData);
+    });
+    infiniteScroll()
+    lightboxScroll();
+}
 
 // NYT API
+
 function getBestSellerImage(result) {
     let thumbnail = 'https://image.ibb.co/bYtXH7/no_cover_en_US.jpg';
     if (result.totalItems > 0 &&
@@ -186,7 +194,6 @@ function getBestSellerISBN(isbns) {
 }
 
 function getBook(isbn) {
-    googleAjaxData.maxResults = 2;
     googleAjaxData.startIndex = 0;
     googleAjaxData.q = "isbn:" + isbn;
     return getBooksFromAPI(GOOGLE_BOOKS_API_URL, googleAjaxData);
@@ -221,106 +228,98 @@ function showBestSeller() {
         </section>
       `);
     });
-    // DATA.view = 'loading';
-    // then we can pass each container and header into displayBestSellerData
     sections.reduce((promise, name) => {
         return promise.then(() => {
             nyAjaxData.list = name;
             return getBooksFromAPI(NYT_API_URL, nyAjaxData, displayBestSellerData(nyAjaxData.list));
         });
-    }, Promise.resolve()).then(() => {
-        //   DATA.view = 'best-seller';
-        //   emit('loaded');
-    });
+    }, Promise.resolve()).then(() => {});
 }
 
 // RENDER
 
 function renderBooks(book) {
     return `
-      <div class="book col">
+    <div class="book col">
         <div class="bookItem w3-animate-opacity">
-          <i class="test fa fa-eye fa-lg"></i>        
-          <i class="fa fa-heart fa-lg"></i>   
-          <div class='star-rating'>
-            <i class="fa fa-star"></i>   
-            <i class="fa fa-star"></i>   
-            <i class="fa fa-star"></i>   
-            <i class="fa fa-star"></i>   
-            <i class="fa fa-star-half-full"></i>   
-            <span>4.5/5</span>         
-          </div>            
-          <a href='#${book.id}'>
-            <img src="${book.thumbnail}" alt=${book.title} book cover>                 
-          </a>
-          <p class="title">${book.title}</p>  
-          
-          <div class="lightbox" id="${book.id}">
-            <div class="lightbox-content">
-              <a href="#_" class="fa fa-close fa-2x"></a>
-              <img src="${book.thumbnail}">
-              <h4>${book.title} <h6>by</h6> <h5>${book.author}</h5></h4>
-              <p class="book-description">${book.snippet}</p>
-            </div>
-          </div>        
+                <i class="test fa fa-eye fa-lg"></i>        
+                <i class="fa fa-heart fa-lg"></i>   
+                <div class='star-rating'>
+                    <i class="fa fa-star"></i>   
+                    <i class="fa fa-star"></i>   
+                    <i class="fa fa-star"></i>   
+                    <i class="fa fa-star"></i>   
+                    <i class="fa fa-star-half-full"></i>   
+                <span>4.5/5</span>         
+            </div>            
+            <a href='#${book.id}'>
+                <img src="${book.thumbnail}" alt=${book.title}>                 
+            </a>
+            <p class="title">${book.title}</p>  
+            
+            <div class="lightbox" id="${book.id}">
+                <div class="lightbox-content">
+                    <a href="#_" class="fa fa-close fa-2x"></a>
+                        <img src="${book.thumbnail}">
+                    <h4>${book.title} <h6>by</h6> <h5>${book.author}</h5></h4>
+                    <p class="book-description">${book.snippet}</p>
+                </div>
+            </div>        
         </div>      
-      </div>        
-      `;
+    </div>        
+    `;
 }
 
 function renderBestSellers(results, thumbnail, ISBN) {
     return `    
-      <div class="book col">
-        <div class="bookItem w3-animate-opacity">        
-          <i class="test fa fa-eye fa-lg"></i>        
-          <i class="fa fa-heart fa-lg"></i>   
-          <div class='star-rating'>
+    <div class="book col">
+    <div class="bookItem w3-animate-opacity">        
+        <i class="test fa fa-eye fa-lg"></i>        
+        <i class="fa fa-heart fa-lg"></i>   
+        <div class='star-rating'>
             <i class="fa fa-star"></i>   
             <i class="fa fa-star"></i>   
             <i class="fa fa-star"></i>   
             <i class="fa fa-star"></i>   
             <i class="fa fa-star-half-full"></i>   
             <span>4.5/5</span>         
-          </div>
-          <a href='#${results.book_details[0].primary_isbn13}'>
-            <img src="${thumbnail}">  
-          </a>  
-          <p class="title">${results.book_details[0].title.toLowerCase()}</p>
         </div>
-  
-        <div class="lightbox" id="${results.book_details[0].primary_isbn13}">
-          <div class="lightbox-content">
+        <a href='#${results.book_details[0].primary_isbn13}'>
+            <img src="${thumbnail}">  
+        </a>  
+        <p class="title">${results.book_details[0].title.toLowerCase()}</p>
+    </div>
+
+    <div class="lightbox" id="${results.book_details[0].primary_isbn13}">
+        <div class="lightbox-content">
             <a href="#_" class="fa fa-close fa-2x"></a>
             <img src="${thumbnail}">
             <h3 class="best-seller-lightbox-title">${results.book_details[0].title.toLowerCase()} <h6>by</h6> <h5>${results.book_details[0].author}</h5></h3>
             <p class="book-description">${results.book_details[0].description}</p>
-          </div>
-        </div>  
-      </div>     
+        </div>
+    </div>  
+</div>     
     `;
 }
-
 
 function renderListName(name) {
     return `
     <div class="book col w3-animate-opacity listName">      
-    <h5>${name}</h5>                  
+        <h5>${name}</h5>                  
     </div>
     `;
 }
-
 
 function renderEmptyForm() {
     $('.book-container').empty();
 }
 
+// ON PAGE LOAD
 
-// page on load
 function onLoadTrigger() {
-    $(watchSubmit());
-    $(speechRecognition());
-    $(infiniteScroll());
-    // $(showBestSeller())
+    initEventHandler();
+    speechRecognition();
+    // showBestSeller();
 }
 
 $(onLoadTrigger());
